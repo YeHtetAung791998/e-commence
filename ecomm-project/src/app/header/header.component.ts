c1aa4982607da69827fa89ef881d4701474dcc72
YeHtetAung791998 <scm.yehtetaung@gmail.com>
2023-02-22 23:16:11 +0630

    add navbar condition

--- ecomm-project/src/app/header/header.component.ts
+++ ecomm-project/src/app/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+   
+  menuType:string = 'default';
+  constructor(private route:Router){}
+  ngOnInit(): void {
+   this.route.events.subscribe((val:any) => {
+    console.log(val.url)
+      if(val.url){
+          if(localStorage.getItem('seller') && val.url.includes('seller')){
+                this.menuType = 'seller'
+                console.log(this.menuType)
+          }
+          else{
+            this.menuType = 'default'
+            console.log(this.menuType)
+          }
+      }
+   })
+  }
+
+      
 
 }
