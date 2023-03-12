332daf4503807faf7faa3883d2c7480cf74a3b77
YeHtetAung791998 <scm.yehtetaung@gmail.com>
2023-03-12 12:12:17 +0630

    create user auth

---
+++ ecomm-project/src/app/user-auth/user-auth.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {SignUp} from '../data-type'
+import { UserService } from '../services/user.service';
+@Component({
+  selector: 'app-user-auth',
+  templateUrl: './user-auth.component.html',
+  styleUrls: ['./user-auth.component.css']
+})
+export class UserAuthComponent implements OnInit{
+
+  constructor(private userService:UserService){}
+  ngOnInit(): void {
+   this.userService.userAuthReload();
+  }
+
+  signUp(data:SignUp){
+      this.userService.userSignup(data)
+  }
+}
